2158b405f620592013595e1bdc5891a8004e918c
Edouard Demotes <e.demotes@gmail.com>
2024-09-17 11:05:38 -0400

    refactor: rename module

--- ift6758/data/__main__.py
+++ ift6758/data/__main__.py
@@ -1,7 +1,7 @@
 """
 This file is for development and testing purpose
 """
-from classes import (ApiClient, FileSystemCache, GameType)
+from api import (ApiClient, FileSystemCache, GameType)
 import os
 
 cache_path = os.path.dirname(os.path.abspath(__file__)) + "/storage/cache"
